626ec11862b5f9dda96d7f8f6fa1689578d42359
GabrielSouzaSi <gos.gabri@gmail.com>
2026-06-08 18:27:08 -0400

    fix: change myOffers route from POST to GET method

--- src/routes/myOffers.ts
+++ src/routes/myOffers.ts
@@ -1,7 +1,7 @@
 import { FastifyInstance } from "fastify";
 
 export function myOffers(app: FastifyInstance) {
-  app.post("/my/offers", async (req, res) => {
+  app.get("/my/offers", async (req, res) => {
     console.log(req.headers.authorization?.split(" ")[1]);
 
     const response = await fetch("http://189.126.105.9:5990/api/my/offers", {
