604f22e07d8df7cc824abb4995725f307e52d6ed
GabrielSouzaSi <gos.gabri@gmail.com>
2026-06-08 17:52:04 -0400

    add loginAcai route and refactor server setup

---
+++ src/routes/loginAcai.ts
@@ -0,0 +1,38 @@
+import { FastifyInstance } from "fastify";
+import { prisma } from "../lib/prisma";
+import { z } from "zod";
+import { verifyPassword } from "../utils/hash";
+
+export function loginAcai(app: FastifyInstance) {
+  const loginUserSchema = z.object({
+    email: z.string().email(),
+    password: z.string(),
+  });
+
+  app.post("/login/acai", async (req, res) => {
+    const { email, password } = loginUserSchema.parse(req.body);
+
+    const response = await fetch("http://189.126.105.9:5990/api/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    });
+
+    if (!response.ok) {
+      return res.status(401).send({
+        message: "Credenciais inválidas",
+      });
+    }
+
+    const data = await response.json();
+
+    return {
+      data,
+    };
+  });
+}
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import fastify from "fastify";
 import fastifyJwt from "fastify-jwt";
-import fastifyCors from "@fastify/cors"
+import fastifyCors from "@fastify/cors";
 
 import { createUser } from "./routes/createUser";
 import { login } from "./routes/login";
 import { profile } from "./routes/profile";
+import { loginAcai } from "./routes/loginAcai";
 
 const app = fastify();
 
@@ -12,18 +13,31 @@ app.register(fastifyCors);
 
 app.register(createUser);
 app.register(login);
+app.register(loginAcai);
 app.register(profile);
 app.get("/", () => {
-    return "Ok"
-})
+  return "Ok";
+});
+app.get("/acai", async () => {
+  const response = await fetch("http://189.126.105.9:5990/api/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const data = await response.json();
+  return data;
+});
 
 app.register(fastifyJwt, {
-    secret: "secret"
-})
+  secret: "secret",
+});
 
-app.listen({
+app
+  .listen({
     host: "0.0.0.0",
-    port: process.env.PORT ? Number(process.env.PORT) : 3333
-}).then(() => {
+    port: process.env.PORT ? Number(process.env.PORT) : 3333,
+  })
+  .then(() => {
     console.log("🚀 HTTP Server Running!");
-})
+  });
