22aa5d5d49ccd3532aa0986e552a2c719f61535e
GabrielSouzaSi <gos.gabri@gmail.com>
2026-06-08 18:24:07 -0400

    fix: update authorization header handling and improve error message

--- src/routes/myOffers.ts
+++ src/routes/myOffers.ts
@@ -2,17 +2,19 @@ import { FastifyInstance } from "fastify";
 
 export function myOffers(app: FastifyInstance) {
   app.post("/my/offers", async (req, res) => {
+    console.log(req.headers.authorization?.split(" ")[1]);
+
     const response = await fetch("http://189.126.105.9:5990/api/my/offers", {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
-        Authorization: `Bearer 25|FiVpHwkkwx4MhSrS5zSQ1XFVUMoFg66klXV8ZhkE8dcc5949`,
+        Authorization: `Bearer ${req.headers.authorization?.split(" ")[1]}`,
       },
     });
 
     if (!response.ok) {
       return res.status(401).send({
-        message: "Algo deu errado",
+        message: "Alguma coisa deu errado",
       });
     }
 
